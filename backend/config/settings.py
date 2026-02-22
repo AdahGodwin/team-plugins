5f0616fafe5e674fd99f8feb1a295c8ba5a8fd8f
Samuel Alawode <71472896+Gaseous01@users.noreply.github.com>
2026-02-22 04:30:36 +0100

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -65,6 +65,12 @@ INSTALLED_APPS = [
     'apps.patients',
     'apps.medical',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
