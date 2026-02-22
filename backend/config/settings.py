ac9f22b270006ac852fe195a70882e1db857abc4
Samuel Alawode <71472896+Gaseous01@users.noreply.github.com>
2026-02-22 04:24:21 +0100

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,20 +24,7 @@ DATABASES = {
         ssl_require=not DEBUG  # Force SSL on Render, disable for local SQLite
     )
 }
-PREREQUISITE_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'drf_spectacular',
-    'whitenoise.runserver_nostatic', # Recommended for Whitenoise
-]
 
-INSTALLED_APPS = PREREQUISITE_APPS + CUSTOM_APPS
 # --- STATIC FILES (WhiteNoise) ---
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
@@ -52,11 +39,71 @@ STORAGES = {
 GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
 
 RENDER = os.environ.get('RENDER') 
-ROOT_URLCONF = 'config.urls'
-WSGI_APPLICATION = 'config.wsgi.application'
+
 if RENDER:
     DEBUG = False
     ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 else:
     DEBUG = True
     ALLOWED_HOSTS = []
+
+
+
+INSTALLED_APPS = [
+    # Django contrib apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Third‑party
+    'drf_spectacular',
+
+    # Local apps
+    'apps.patients',
+    'apps.medical',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# Internationalisation
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
